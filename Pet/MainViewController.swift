a20771246edaf0dfdaed977b0327c2817294f7b6
JulyBelova <july@belova.es>
2024-02-12 23:16:47 +0300

    Удалила сториборд

--- Pet/MainViewController.swift
+++ Pet/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Pet
 //
 //  Created by July Belova on 12.02.2024.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
     }
 
 
